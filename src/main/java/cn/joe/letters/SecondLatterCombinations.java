9ea2646be9a266da9ddb7f4e3c46f33b6b9968a5
zhouqizhou <zhouqizhou@outlook.com>
2020-09-13 16:25:26 +0800

    optimize

--- src/main/java/cn/joe/letters/SecondLatterCombinations.java
+++ src/main/java/cn/joe/letters/SecondLatterCombinations.java
@@ -13,10 +13,15 @@ public class SecondLatterCombinations extends LatterCombinations {
 
 			throw new IllegalArgumentException("Illegal param[" + index + "].Please control between 0-99");
 		} else {
-
-			int tens = index / 10;
-			int bits = index % 10;
-			return table[tens] + table[bits];
+			if(index < table.length) {
+				
+				return table[index];
+			}else {
+				
+				int tens = index / 10;
+				int bits = index % 10;
+				return table[tens] + table[bits];
+			}
 		}
 	}
 
